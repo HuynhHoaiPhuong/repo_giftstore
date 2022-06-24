c7231fc29ba74d44bc7e2d00febff215fb4784f1
ngotuongbao <ngotuongbao3@gmail.com>
2022-06-24 21:42:04 +0700

    update migration filename by ntb

--- giftstore_website/app/Models/Bill_Order_Detail.php
+++ giftstore_website/app/Models/Bill_Order_Detail.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Product;
 use App\Models\Bill_Order;
-class Bill_Order_Details extends Model
+class Bill_Order_Detail extends Model
 {
     use HasFactory;
     protected $fillable = [
