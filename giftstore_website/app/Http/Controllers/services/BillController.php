23b80cf07b66b836d5fdc137087759309a8d0a6a
ngotuongbao <ngotuongbao3@gmail.com>
2022-10-25 12:35:27 +0700

    edit foreign key by ntb

--- giftstore_website/app/Http/Controllers/services/BillController.php
+++ giftstore_website/app/Http/Controllers/services/BillController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\services;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Bill;
+use Carbon\Carbon;
+use App\Http\Resources\BillResource;
+use App\Http\Payload;
 
 class BillController extends Controller
 {
-    //
+    public function getAllRankByStatus($status)
+    {
+        $bills = Bill::where([
+                ['status', $status]
+            ])->orderBy('id_bill','ASC')->get();
+         if($bills->isEmpty())
+            return Payload::toJson(null,"Data Not Found",404);   
+        return Payload::toJson(BillResource::collection($bills),"Request Successfully",200);
+    }
 }
--- giftstore_website/app/Http/Controllers/services/StockController.php
+++ giftstore_website/app/Http/Controllers/services/StockController.php
@@ -4,8 +4,63 @@ namespace App\Http\Controllers\services;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Stock;
+use Carbon\Carbon;
+use App\Http\Resources\StockResource;
+use App\Http\Payload;
 
 class StockController extends Controller
 {
-    //
+    public function getAllStockByStatus($status)
+    {
+        $stocks = Stock::where([
+                ['status', $status]
+            ])->orderBy('name','ASC')->get();
+         if($stocks->isEmpty())
+            return Payload::toJson(null,"Data Not Found",404);   
+        return Payload::toJson(StockResource::collection($stocks),"Request Successfully",200);
+    }
+    public function saveStock(Request $req)
+    {
+        $stock = new Stock();
+        $stock->fill(
+            [
+                'id_stock' =>  $req -> id_stock,
+                'name' => $req->name,
+                'address' => $req->address,
+            ]
+        );
+        $stock->save();
+        $stock = Stock::where('id_stock',$stock->id_stock)->first();
+        return Payload::toJson(new StockResource($stock),"Create Successfully",201);
+    }
+
+    public function updateStock(Request $req)
+    {
+        $result = Stock::where('id_stock', $req -> id_stock)
+            //Key Value // Get e by array...
+            ->update(
+                [
+                    'name' => $req->name,
+                    'address' => $req->address,
+                ],
+            );  
+        if($result == 1){
+            $stock = Stock::where('id_stock',$req->id_stock)->first();
+            return Payload::toJson(new StockResource($stock),"Update Successfully",202);
+        }
+        return Payload::toJson(null,"Cannot Update",500);
+    }
+
+    public function removeStock(Request $req)
+    {
+        $result = Stock::where('id_stock', $req -> id_stock)
+             ->update(['status'=> $req -> status]);
+        if($result == 1)
+        {
+            $stock = Stock::where('id_stock',$req->id_stock)->first();
+            return Payload::toJson(new StockResource($stock),"Remove Successfully",202);
+        }
+        return Payload::toJson(null,"Cannot Update",500);
+    }
 }
