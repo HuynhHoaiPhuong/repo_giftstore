412bb7b0f521394e64a839463278f725c6cacd86
YIDARKMASTER <huynhaiphuong001@gmail.com>
2023-05-09 05:45:43 +0700

    update api 12 last table

--- giftstore_website/routes/api.php
+++ giftstore_website/routes/api.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\services\WarehouseDetailController;
 use App\Http\Controllers\services\ProviderController;
 use App\Http\Controllers\services\BillOrderController;
 use App\Http\Controllers\services\FavoriteController;
-use App\Http\Controllers\services\StaticPageController;
+use App\Http\Controllers\services\PostController;
 use App\Http\Controllers\services\TopicController;
 use App\Http\Controllers\services\SettingController;
 use App\Http\Controllers\services\VoucherController;
@@ -26,6 +26,7 @@ use App\Http\Controllers\services\ActivityHistoryController;
 use App\Http\Controllers\services\UserController;
 use App\Http\Controllers\services\MemberController;
 use App\Http\Controllers\services\PhotoController;
+use App\Http\Controllers\services\PaymentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -134,84 +135,46 @@ Route::group(['prefix'=>'/bill-orders'],function() {
 
 //--------------------End 12 Head Tables--------------------------
 
-//Bill API
-Route::group(['prefix'=>'/bills'],function() {
-    Route::get('/get-all-bill-by-status/{status}',[BillController::class,'getAllBillByStatus']);
-    Route::post('/save-bill',[BillController::class,'store']);
-    // Route::post('/update-bill',[BillController::class,'update']);
-    // Route::post('/remove-bill',[BillController::class,'destroy']);
-});
-//Bill-Detail API
-Route::group(['prefix'=>'/bill-details'],function() {
-    Route::get('/get-all-bill-detail-by-status/{status}',[BillDetailController::class,'getAllBillDetailByStatus']);
-    Route::post('/save-bill-detail',[BillDetailController::class,'store']);
-    Route::post('/update-bill-detail',[BillDetailController::class,'update']);
-    Route::post('/remove-bill-detail',[BillDetailController::class,'destroy']);
-});
-
 //Bill-Order-Detail API
 Route::group(['prefix'=>'/bill-order-details'],function() {
     Route::get('/get-all-bill-order-detail-by-status/{status}',[BillOrderDetailController::class,'getAllBillOrderDetailByStatus']);
-    Route::post('/save-bill-order-detail',[BillOrderDetailController::class,'store']);
-    Route::post('/update-bill-order-detail',[BillOrderDetailController::class,'update']);
-    Route::post('/remove-bill-order-detail',[BillOrderDetailController::class,'destroy']);
+    Route::post('/save-bill-order-detail',[BillOrderDetailController::class,'saveBillOrderDetail']);
 });
 
-//Discount API
-Route::group(['prefix'=>'/discounts'],function() {
-    Route::get('/get-all-discount-by-status/{status}',[DiscountController::class,'getAllDiscountByStatus']);
-    Route::post('/save-discount',[DiscountController::class,'store']);
-    Route::post('/update-discount',[DiscountController::class,'update']);
-    Route::post('/remove-discount',[DiscountController::class,'destroy']);
+//Bill API
+Route::group(['prefix'=>'/bills'],function() {
+    Route::get('/get-all-bill-by-status/{status}',[BillController::class,'getAllBillByStatus']);
+    Route::post('/save-bill',[BillController::class,'saveBill']);
+});
+
+//Bill-Detail API
+Route::group(['prefix'=>'/bill-details'],function() {
+    Route::get('/get-all-bill-detail-by-status/{status}',[BillDetailController::class,'getAllBillDetailByStatus']);
+    Route::post('/save-bill-detail',[BillDetailController::class,'saveBillDetail']);
+});
+
+//API payment
+Route::group(['prefix'=>'/payments'],function() {
+    Route::get('/get-all-payment-by-status/{status}',[PaymentController::class,'getAllPaymentByStatus']);
+    Route::post('/save-payment',[PaymentController::class,'savePayment']);
+    Route::post('/update-payment',[PaymentController::class,'updatePayment']);
+    Route::post('/remove-payment',[PaymentController::class,'removePayment']);
 });
 
 //Voucher API
 Route::group(['prefix'=>'/vouchers'],function() {
     Route::get('/get-all-voucher-by-status/{status}',[VoucherController::class,'getAllVoucherByStatus']);
-    Route::post('/save-voucher',[VoucherController::class,'store']);
-    Route::post('/update-voucher',[VoucherController::class,'update']);
-    Route::post('/remove-voucher',[VoucherController::class,'destroy']);
+    Route::post('/save-voucher',[VoucherController::class,'saveVoucher']);
+    Route::post('/update-voucher',[VoucherController::class,'updateVoucher']);
+    Route::post('/remove-voucher',[VoucherController::class,'removeVoucher']);
 });
 
-//Activity-History API
-Route::group(['prefix'=>'/activities-history'],function() {
-    Route::get('/get-all-activity-history-by-status/{status}',[activityHistoryController::class,'getAllActivityHistoryByStatus']);
-    Route::post('/save-activity-history',[activityHistoryController::class,'store']);
-    Route::post('/update-activity-history',[activityHistoryController::class,'update']);
-    Route::post('/remove-activity-history',[activityHistoryController::class,'détroy']);
-});
-
-// API user
-Route::group(['prefix'=>'/users'],function()
-{
-    Route::get('/get-all-user-by-status/{status}',[UserController::class,'getAllUserByStatus']);
-    Route::post('/save-user',[UserController::class,'saveUser']);
-    Route::post('/update-user',[UserController::class,'updateUser']);
-    Route::post('/remove-user',[UserController::class,'removeUser']);
-});
-
-// API setting
-Route::group(['prefix'=>'/settings'],function()
-{
-    Route::get('/get-all-setting-by-status/{status}',[SettingController::class,'getAllSettingByStatus']);
-   
-    Route::post('/save-setting',[SettingController::class,'saveSetting']);
-   
-    Route::post('/update-setting',[SettingController::class,'updateSetting']);
-   
-    Route::post('/remove-setting',[SettingController::class,'removeSetting']);
-});
-
-// API static
-Route::group(['prefix'=>'/static_pages'],function()
-{
-    Route::get('/get-all-static-by-status/{status}',[StaticPageController::class,'getAllStaticByStatus']);
-   
-    Route::post('/save-static',[StaticPageController::class,'saveStatic']);
-   
-    Route::post('/update-static',[StaticPageController::class,'updateStatic']);
-   
-    Route::post('/remove-static',[StaticPageController::class,'removeStatic']);
+//Discount API
+Route::group(['prefix'=>'/discounts'],function() {
+    Route::get('/get-all-discount-by-status/{status}',[DiscountController::class,'getAllDiscountByStatus']);
+    Route::post('/save-discount',[DiscountController::class,'saveDiscount']);
+    Route::post('/update-discount',[DiscountController::class,'updateDiscount']);
+    Route::post('/remove-discount',[DiscountController::class,'removeDiscount']);
 });
 
 // API topic
@@ -226,16 +189,28 @@ Route::group(['prefix'=>'/topics'],function()
     Route::post('/remove-topic',[TopicController::class,'removeTopic']);
 });
 
-// API photo
-Route::group(['prefix'=>'/photos'],function()
+// API post
+Route::group(['prefix'=>'/posts'],function()
 {
-    Route::get('/get-all-photo-by-status/{status}',[PhotoController::class,'getAllPhotoByStatus']);
+    Route::get('/get-all-post-by-status/{status}',[PostController::class,'getAllPostByStatus']);
    
-    Route::post('/save-photo',[PhotoController::class,'savePhoto']);
+    Route::post('/save-post',[PostController::class,'savePost']);
    
-    Route::post('/update-photo',[PhotoController::class,'updatePhoto']);
+    Route::post('/update-post',[PostController::class,'updatePost']);
    
-    Route::post('/remove-photo',[PhotoController::class,'removePhoto']);
+    Route::post('/remove-post',[PostController::class,'removePost']);
+});
+
+// API user
+Route::group(['prefix'=>'/users'],function()
+{
+    Route::get('/get-all-user-by-status/{status}',[UserController::class,'getAllUserByStatus']);
+    
+    Route::post('/save-user',[UserController::class,'saveUser']);
+
+    Route::post('/update-user',[UserController::class,'updateUser']);
+
+    Route::post('/remove-user',[UserController::class,'removeUser']);
 });
 
 // API member
@@ -248,4 +223,40 @@ Route::group(['prefix'=>'/members'],function()
     Route::post('/update-current-point-member',[MemberController::class,'updateCurrentPointMember']);
    
     Route::post('/remove-member',[MemberController::class,'removeMember']);
+});
+
+// API photo
+Route::group(['prefix'=>'/photos'],function()
+{
+    Route::get('/get-all-photo-by-status/{status}',[PhotoController::class,'getAllPhotoByStatus']);
+   
+    Route::post('/save-photo',[PhotoController::class,'savePhoto']);
+   
+    Route::post('/update-photo',[PhotoController::class,'updatePhoto']);
+   
+    Route::post('/remove-photo',[PhotoController::class,'removePhoto']);
+});
+
+// API setting
+Route::group(['prefix'=>'/settings'],function()
+{
+    Route::get('/get-all-setting-by-status/{status}',[SettingController::class,'getAllSettingByStatus']);
+   
+    Route::post('/save-setting',[SettingController::class,'saveSetting']);
+   
+    Route::post('/update-setting',[SettingController::class,'updateSetting']);
+   
+    Route::post('/remove-setting',[SettingController::class,'removeSetting']);
+});
+
+//Activity-History API
+Route::group(['prefix'=>'/activities-history'],function()
+{
+    Route::get('/get-all-activity-history-by-status/{status}',[activityHistoryController::class,'getAllActivityHistoryByStatus']);
+    
+    Route::post('/save-activity-history',[activityHistoryController::class,'store']);
+    
+    Route::post('/update-activity-history',[activityHistoryController::class,'update']);
+    
+    Route::post('/remove-activity-history',[activityHistoryController::class,'détroy']);
 });
