616e2ae610ebbf296bba00ccb743ce183883e70e
ngotuongbao <ngotuongbao3@gmail.com>
2023-07-07 14:25:23 +0700

    paginate for shop client

--- giftstore_website/app/Http/Controllers/user/ShopController.php
+++ giftstore_website/app/Http/Controllers/user/ShopController.php
@@ -11,11 +11,39 @@ class ShopController extends Controller
     public function index(){
         $productController = new ServicesProductController();
         $data_product = $productController->getAllProductByStatus('enabled');
-        $products = [];
-        if($data_product['data']!=null)
-            $products = $data_product['data']->collection;
-        return view('user/templates/shop',[
+
+        if($data_product['data'] != null)
+        $products = $data_product['data']->collection;
+
+        $products = collect([]);
+        $perPage = 12;
+        $currentPage = max(1, request()->input('page', 1));
+
+        $offset = ($currentPage - 1) * $perPage;
+
+        $paginatedContacts = collect($products)->slice($offset, $perPage);
+
+        $lastPage = ceil($products->count() / $perPage);
+        $startPage = max(1, $currentPage - 2);
+        $endPage = min($lastPage, $currentPage + 2);
+    
+        if ($startPage === 1 && $currentPage === 1)
+            $endPage = min($lastPage, 3);
+        
+        $endPage = min($lastPage, $startPage + 4);
+    
+        $products = $products->forPage($currentPage, $perPage);
+
+        return view('user/templates/shop', [
             'products' => $products,
+            'paginatedContacts' => $paginatedContacts,
+            'pagination' => [
+                'perPage' => $perPage,
+                'currentPage' => $currentPage,
+                'lastPage' => $lastPage,
+                'startPage' => $startPage,
+                'endPage' => $endPage,
+            ],
         ]);
     }
 }
